88157085d5ed6546c2de93ad7032eb5ff6252f3b
shorly <li_xiaoliang@foxmail.com>
2015-06-11 18:14:31 +0800

    update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ var config={
     username:'leebox',
     password:'123',
 
-    db:'mongodb://127.0.0.1/myblog',
-    //db:'mongodb://leebox:519261612@ds045097.mongolab.com:45097/leebox',
+    // db:'mongodb://127.0.0.1/myblog',
+    db:'mongodb://leebox:519261612@ds045097.mongolab.com:45097/leebox',
     crypto:"md5",
     passkey:"my_blog"
 }
